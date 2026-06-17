b52ac9d6cca89acf36d6c86d4704e63919fd113d
dthakare9370 <dthakare9370@gmail.com>
2026-06-17 23:36:55 +0530

    ..

--- pattern/Pattern20.java
+++ pattern/Pattern20.java
@@ -1,6 +1,6 @@
 class Pattern20{
     public static void main(String args[]){
-        int n = 5;
+        int n = 10;
 
         for(int i=1;i<=n;i++){
             //star
@@ -22,17 +22,17 @@ class Pattern20{
 
         for(int i=n+1;i<=n*2-1;i++){
             //star
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=2*n-i;j++){
                 System.out.print("*");
             }
 
-            //space
-            for(int j=1;j<=2*(n-i);j++){
+            // //space
+            for(int j=1;j<=2*(i-n);j++){
                 System.out.print(" ");
             }
 
-            //star
-            for(int j=1;j<=i;j++){
+            // //star
+            for(int j=1;j<=2*n-i;j++){
                 System.out.print("*");
             }
             System.out.println();
